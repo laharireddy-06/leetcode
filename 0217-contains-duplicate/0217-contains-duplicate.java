0f00435c86e0d5a04b363a934749d02bb3714c1a
laharireddy-06 <vtu24893@veltech.edu.in>
2026-02-04 22:01:09 +0530

    Time: 15 ms (64.86%), Space: 96.1 MB (23.32%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int current = nums[i];
+
+            if (map.containsKey(current)) {
+                return true;
+            }
+
+            map.put(current, 1);
+        }
+
+        return false;
+    }
+}
